b87b72279c7bd6f265531a18771d6640aa9145fd
BlueDark67 <guijfpedro@gmail.com>
2025-04-05 00:49:36 +0100

    EmailSent page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from "../views/HomePage/HomePage.jsx";
 import ForgotPassword from "../views/ForgotPassword/ForgotPassword.jsx";
 import LoginPage from "../views/LoginPage/LoginPage";
 import PersonalData from "../views/UserRelatedPage/Personaldata/PersonalData.jsx";
+import EmailSent from "../views/EmailSent/EmailSent.jsx";
 
 function App(){
     return(
@@ -14,6 +15,8 @@ function App(){
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/personal-data" element={<PersonalData />} />
+                <Route path="/email-sent" element={<EmailSent />} />
+                <Route path="*" element={<div>404 Not Found</div>} />
             </Routes>
         </BrowserRouter>
     );
