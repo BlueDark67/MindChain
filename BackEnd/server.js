381227576d18ae756d557d392e010ebf74b54925
BlueDark67 <guijfpedro@gmail.com>
2025-04-07 11:19:16 +0100

    BackEnd working

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,15 +2,36 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import http from "http";
-import reactEngine from "express-react-views";
+import cors from "cors";
 import homeRoutes from "./routes/homeRoutes.js";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const names = {
+  names: [
+    "Anna",
+    "Bob",
+    "Charlie",
+    "David",
+    "Eve",
+    "Frank",
+    "Grace",
+    "Heidi",
+    "Ivan",
+    "Judy",
+  ],
+};
+
 app.get("/", homeRoutes);
+
+app.get("/users", (req, res) => {
+  res.json(names);
+});
 
 app.use(express.urlencoded({ extended: true }));
 
