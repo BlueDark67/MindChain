469610b0ce568e1539b952720be9760550e1a976
BlueDark67 <guijfpedro@gmail.com>
2025-05-05 21:03:00 +0100

    Update envio email e erro no forgotpassword

--- BackEnd/controllers/roomController.js
+++ BackEnd/controllers/roomController.js
@@ -92,8 +92,8 @@ const sendEmailInviteRoom = async (req, res) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD,
